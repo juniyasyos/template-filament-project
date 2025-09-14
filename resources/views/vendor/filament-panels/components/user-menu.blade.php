bce03cd91bc5b9855f5d9525e3b57d483f8b4136
juniyasyos <juniyasyos@gmail.com>
2025-09-14 13:10:35 +0700

    feat: update style dark

--- resources/views/vendor/filament-panels/components/user-menu.blade.php
+++ resources/views/vendor/filament-panels/components/user-menu.blade.php
@@ -7,36 +7,30 @@
     $items = $this->getUserMenuItems();
 
     $itemsBeforeAndAfterThemeSwitcher = collect($items)
-        ->groupBy(fn (Action $item): bool => $item->getSort() < 0, preserveKeys: true)
+        ->groupBy(fn(Action $item): bool => $item->getSort() < 0, preserveKeys: true)
         ->all();
     $itemsBeforeThemeSwitcher = $itemsBeforeAndAfterThemeSwitcher[true] ?? collect();
     $itemsAfterThemeSwitcher = $itemsBeforeAndAfterThemeSwitcher[false] ?? collect();
 
-    $hasProfileHeader = $itemsBeforeThemeSwitcher->has('profile') &&
+    $hasProfileHeader =
+        $itemsBeforeThemeSwitcher->has('profile') &&
         blank(($item = Arr::first($itemsBeforeThemeSwitcher))->getUrl()) &&
-        (! $item->hasAction());
+        !$item->hasAction();
 
     if ($itemsBeforeThemeSwitcher->has('profile')) {
-        $itemsBeforeThemeSwitcher = $itemsBeforeThemeSwitcher->prepend($itemsBeforeThemeSwitcher->pull('profile'), 'profile');
+        $itemsBeforeThemeSwitcher = $itemsBeforeThemeSwitcher->prepend(
+            $itemsBeforeThemeSwitcher->pull('profile'),
+            'profile',
+        );
     }
 @endphp
 
 {{ \Filament\Support\Facades\FilamentView::renderHook(\Filament\View\PanelsRenderHook::USER_MENU_BEFORE) }}
 
-<x-filament::dropdown
-    placement="bottom-end"
-    teleport
-    :attributes="
-        \Filament\Support\prepare_inherited_attributes($attributes)
-            ->class(['fi-user-menu'])
-    "
->
+<x-filament::dropdown placement="bottom-end" teleport :attributes="\Filament\Support\prepare_inherited_attributes($attributes)->class(['fi-user-menu'])">
     <x-slot name="trigger">
-        <button
-            aria-label="{{ __('filament-panels::layout.actions.open_user_menu.label') }}"
-            type="button"
-            class="fi-user-menu-trigger"
-        >
+        <button aria-label="{{ __('filament-panels::layout.actions.open_user_menu.label') }}" type="button"
+            class="fi-user-menu-trigger">
             <x-filament-panels::avatar.user :user="$user" loading="lazy" />
         </button>
     </x-slot>
@@ -75,7 +69,7 @@
         </x-filament::dropdown.list>
     @endif
 
-    @if (filament()->hasDarkMode() && (! filament()->hasDarkModeForced()))
+    @if (filament()->hasDarkMode() && !filament()->hasDarkModeForced())
         <x-filament::dropdown.list>
             <x-filament-panels::theme-switcher />
         </x-filament::dropdown.list>
